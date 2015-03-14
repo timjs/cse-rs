a05ffad9d86ec35a12e0977529290fd07d06b8d6
Tim Steenvoorden <tim.steenvoorden@gmail.com>
2015-03-14 12:54:26 +0100

    use if let and while let syntax and function pointers

--- src/main.rs
+++ src/main.rs
@@ -50,18 +50,21 @@ impl<'a> Parser<'a> {
         Parser{chars: s.chars().peekable()}
     }
 
-    fn name(&mut self) -> Name {
+    fn take_while<F: Fn(char) -> bool>(&mut self, pred: F) -> String {
         let mut name = String::new();
-        loop {
-            match self.chars.peek() {
-                Some(&c) if c.is_alphabetic() => {
-                    name.push(c);
-                    self.chars.next();
-                },
-                _ => break,
+        while let Some(&c) = self.chars.peek() {
+            if pred(c) {
+                name.push(c);
+                self.chars.next();
+            } else {
+                break;
             }
         }
         name
+    }
+
+    fn name(&mut self) -> Name {
+        self.take_while(CharExt::is_alphabetic)
     }
 
     fn var(&mut self, name: Name) -> Expr {
@@ -87,10 +90,6 @@ impl<'a> Parser<'a> {
         }
     }
 
-    fn parse(&mut self) -> Expr {
-        self.expr()
-    }
-
 }
 
 // Elimination
@@ -110,6 +109,24 @@ impl<'a> State<'a> {
 
 impl Expr {
 
+    // fn cse<'a>(&'a self, state: &mut State<'a>) -> Expr {
+    //     if let Some(&repl) = state.map.get(self) {
+    //         Sub(repl)
+    //     } else {
+    //         state.map.insert(&self, state.num);
+    //         state.num += 1;
+    //         match *self {
+    //             App(ref n, ref l, ref r) => {
+    //                 let l_ = l.cse(state);
+    //                 let r_ = r.cse(state);
+    //                 App(n.clone(), Box::new(l_), Box::new(r_))
+    //             },
+    //             Var(ref n) => Var(n.clone()),
+    //             Sub(_) => unreachable!(),
+    //         }
+    //     }
+    // }
+
     fn cse<'a>(&'a self, state: &mut State<'a>) -> Expr {
         match state.map.entry(self) {
             Occupied(e) => {
@@ -118,6 +135,7 @@ impl Expr {
             Vacant(e) => {
                 e.insert(state.num);
                 state.num += 1;
+                // match on self has to be out of this match to satisfy the borrowchecker
             }
         }
         match *self {
@@ -139,12 +157,18 @@ fn main() {
     let mut lines = BufReader::new(io::stdin()).lines();
     lines.next(); // line_count
 
-    for read in lines {
-        let line = read.unwrap();
+    // for read in lines {
+    while let Some(Ok(line)) = lines.next() {
+        // let line = read.unwrap();
+        println!(">> Creating parser");
         let mut parser = Parser::new(&line);
-        let expr = parser.parse();
+        println!(">> Parsing");
+        let expr = parser.expr();
+        println!(">> Creating state");
         let mut state = State::new();
+        println!(">> Eliminating");
         let result = expr.cse(&mut state);
         println!("{}", result);
     }
 }
+
